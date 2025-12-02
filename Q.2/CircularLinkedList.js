9a319bfa46f3e5b0c407e57d15287861f85e9813
Nathan Bezerra <nathangamerb6@gmail.com>
2025-12-02 20:28:06 -0300

    implementação dos métodos acessórios

--- Q.2/CircularLinkedList.js
+++ Q.2/CircularLinkedList.js
@@ -1,9 +1,29 @@
+/**
+ * 1. Classe CircularLinkedList
+ * Implementa uma lista circular simples.
+ */
 module.exports = class CircularLinkedList {
     constructor() {
+        //ponteiros principais
         this.head = null
         this.tail = null
         this.count = 0
         this.noAtual = null
-        //ponteiros principais
+        
+    }
+//MÉTODOS ACESSÓRIOS//
+    isEmpty(){
+        this.count === 0 //verifica se é igual em tipo e valor
+    }
+
+    size(){
+        return this.count //retorna a quantidade de elementos presentes na lista
+    }
+    
+    clear(){
+        this.head = null
+        this.tail = null
+        this.count = 0
+        this.noAtual = null  //limpa os atributos. retornando ao seu estágio inicial
     }
 }
