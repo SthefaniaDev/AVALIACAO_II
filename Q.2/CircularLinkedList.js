d9e34b63c76db1129107f4d28be3a6fda4d2d161
Nathan Bezerra <nathangamerb6@gmail.com>
2025-12-02 11:19:03 -0300

    CircularLinkedList

---
+++ Q.2/CircularLinkedList.js
@@ -0,0 +1,9 @@
+module.exports = class CircularLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.count = 0
+        this.noAtual = null
+        //ponteiros principais
+    }
+}
